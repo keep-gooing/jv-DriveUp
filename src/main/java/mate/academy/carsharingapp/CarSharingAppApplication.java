7c8bc20bd6f461176c09d29dbd79d5affd41c6d8
Olha <olha.oleshchuk@gmail.com>
2025-08-01 12:24:44 +0300

    revert changes

--- src/main/java/mate/academy/carsharingapp/CarSharingAppApplication.java
+++ src/main/java/mate/academy/carsharingapp/CarSharingAppApplication.java
@@ -1,6 +1,5 @@
 package mate.academy.carsharingapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarSharingAppApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
-        System.setProperty("TELEGRAM_BOT_TOKEN", dotenv.get("TELEGRAM_BOT_TOKEN"));
-        System.setProperty("STRIPE_PUBLIC_KEY", dotenv.get("STRIPE_PUBLIC_KEY"));
-        System.setProperty("STRIPE_SECRET_KEY", dotenv.get("STRIPE_SECRET_KEY"));
-        System.setProperty("PAYMENT_SUCCESS_URL", dotenv.get("PAYMENT_SUCCESS_URL"));
-        System.setProperty("PAYMENT_CANCEL_URL", dotenv.get("PAYMENT_CANCEL_URL"));
         SpringApplication.run(CarSharingAppApplication.class, args);
     }
 }
