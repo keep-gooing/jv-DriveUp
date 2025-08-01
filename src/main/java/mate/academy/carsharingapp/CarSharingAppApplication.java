3b44f38283decf86a24e74c7571af7f85453ee21
Olha <olha.oleshchuk@gmail.com>
2025-08-01 11:07:02 +0300

    Stop tracking .env

--- src/main/java/mate/academy/carsharingapp/CarSharingAppApplication.java
+++ src/main/java/mate/academy/carsharingapp/CarSharingAppApplication.java
@@ -1,5 +1,6 @@
 package mate.academy.carsharingapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarSharingAppApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
         SpringApplication.run(CarSharingAppApplication.class, args);
     }
-
 }
